d64064e4b6a598edaddf7466ef498b3177ad135f
jaha7 <47249045+jaha7@users.noreply.github.com>
2020-02-21 08:06:39 +0100

    Lagernamnfix & hela geometrin

--- models/pgdefault.js
+++ models/pgdefault.js
@@ -6,15 +6,16 @@ var pgDefault = function pgDefault(queryString, queryOptions, defaultLimit) {
   var sqlSearchField = searchField ? table + '."' + searchField + '" AS "NAMN",' : "";
   var fields = queryOptions.fields;
   var geometryField = queryOptions.geometryName || "geom";
-  var centroid = 'ST_AsText(ST_PointOnSurface(' + table + '."' + geometryField + '")) AS "GEOM" ';
+//var centroid = 'ST_AsText(ST_PointOnSurface(' + table + '."' + geometryField + '")) AS "GEOM" ';
+  var centroid = 'ST_AsText(geom) AS "GEOM"';
   var sqlFields = fields ? fields.join(',') + "," : "";
   var type = " '" + table + "'" + ' AS "TYPE", ';
   var condition = queryString;
   var searchString;
   var limitNumber = queryOptions.limit || defaultLimit || 1000;
   var limit = ' LIMIT ' + limitNumber.toString() + ' ';
-  var layerNamne = queryOptions.layer;
-  var layer = "'" + layerNamne + "' AS LAYER, ";
+  var layerNamn = queryOptions.layer;
+  var layer = " '" + layerNamn + "'" + ' AS "LAYER", ';
 
   searchString =
     'SELECT ' +
