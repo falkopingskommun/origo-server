be1222e5a87ad831d18da34244df4254a33836b6
jaha7 <47249045+jaha7@users.noreply.github.com>
2020-04-02 09:15:43 +0200

    Excelexport

---
+++ handlers/excelcreator.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var router = express.Router();
+var excel = require('exceljs');
+var cors = require('cors')
+
+router.use(cors());
+
+router.post('/', postHandler);
+
+function postHandler(req, res) {
+
+  var workbook = new excel.Workbook(); //creating workbook
+  var sheet = workbook.addWorksheet('MySheet'); //creating worksheet
+
+  var objArray = req.body;
+
+  Object.entries(objArray).forEach(entry => {
+    sheet.addRow([entry[0]]);
+    sheet.addRow(Object.keys(entry[1][0]));
+
+    entry[1].forEach(function (item) {
+      sheet.addRow(Object.values(item));
+    });
+
+    sheet.addRow(); // add an empty row between each layer
+  });
+
+  var options = {
+    // root: __dirname + '/../',
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  };
+
+  var fileName = './public/temp.xlsx';
+
+  // Timeout is set for testing delay spinners in the client, should be removed after test is complete.
+  setTimeout(() => {
+    workbook.xlsx.writeFile(fileName)
+      .then(function () {
+        console.log("file is written");
+        res.setHeader('content-type', 'application/vnd.ms-excel');
+        res.statusMessage = "Custom Status Message";
+        res.customName = "File Name";
+        res.download(fileName, 'ExportedFeatures.xlsx', options, function (err) {
+          if (err) {
+            console.log('Error sending file: ', err);
+          } else {
+            console.log('Sent: ', fileName);
+          }
+        });
+      })
+      .catch(function (err) {
+        console.log("Error writing file.");
+        console.log(err);
+      });
+  }, 3000);
+}
+
+module.exports = router;
